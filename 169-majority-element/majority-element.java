406b15d82bd4bc717cdea5a422ce96a4e541a9e2
Nishank Bansal <80453652+nishank23@users.noreply.github.com>
2025-09-07 11:51:30 +0200

    Time: 1 ms (99.82%) | Memory: 53.1 MB (50.33%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
-   //Boyee Moree Voting Algorithim
-   int candidate = nums[0];
+    int candidate =nums[0];
+    int count = 1;
+    for(int i = 1;i<nums.length;i++){
+        if(count==0){
+           candidate = nums[i];
+           count =1;
+        }
+        else if(nums[i]==candidate){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    return candidate;
+}}
+ //Boyee Moree Voting Algorithim
+/*
+int candidate = nums[0];
    int count = 1;
    for(int i = 1;i<nums.length;i++){
       if(count ==0){
@@ -17,7 +33,11 @@ class Solution {
     return candidate;    
        
     }
-}
+
+*/    
+
+
+
 
  /* 
  Using Hasmap we solved it 
