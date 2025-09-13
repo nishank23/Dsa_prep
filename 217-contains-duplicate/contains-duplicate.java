1911e2ff2bcc81ce0b7bd2d19e0656edc8397fb3
Nishank Bansal <80453652+nishank23@users.noreply.github.com>
2025-09-13 11:52:47 +0200

    Time: 21 ms (20.01%) | Memory: 58.6 MB (39.52%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for(int i = 1;i<nums.length;i++){
+        for(int i =1;i<nums.length;i++){
             if(nums[i]==nums[i-1]){
                 return true;
             }
         }
         return false;
+
+
+
     }
 }
 
-/*
 
-Arrays.sort(nums);
+
+
+
+/*
+        Arrays.sort(nums);
         int first = nums[0];
         for(int i =1;i<nums.length;i++){    
             if(first==nums[i])
@@ -23,6 +29,4 @@ Arrays.sort(nums);
 
         }
         return false;
-
-
 */        
