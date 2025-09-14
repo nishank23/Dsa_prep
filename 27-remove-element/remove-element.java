83cc364ee43b4135ec72f8c24725ff0073710b92
Nishank Bansal <80453652+nishank23@users.noreply.github.com>
2025-09-14 08:28:58 +0200

    Time: 0 ms (100.00%) | Memory: 41.8 MB (84.39%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,6 +1,21 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k =0;
+       int n =nums.length;
+       int k =0;
+       for(int i = 0;i<n;i++){
+            if(nums[i]!=val){
+                int temp = nums[i];
+                nums[i] = nums[k];
+                nums[k] = temp;
+                k++;
+            }
+       }
+       return k;
+    }
+}
+
+/*
+int k =0;
         int i =0;
         int n = nums.length;
         while(i<n){
@@ -12,5 +27,5 @@ class Solution {
         }
         return k;
 
-    }
-}
+
+*/        
