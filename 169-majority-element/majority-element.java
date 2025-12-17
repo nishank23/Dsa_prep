ede79c151374cc50c06392cc7687685d0a487f92
Nishank Bansal <80453652+nishank23@users.noreply.github.com>
2025-12-17 07:50:51 +0200

    Time: 1 ms (99.84%) | Memory: 55.8 MB (19.14%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,64 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-    int candidate = nums[0];
-    int count =1;
-    for(int i =1;i<nums.length;i++){
-        if(candidate==nums[i]){
-            candidate = nums[i];
-            count++;
-        }else if(count ==0){
-            count=0;
-            candidate = nums[i];
-        }else{
-            count--;
-        }
+        int candidate = nums[0];
+        int count = 1;
 
-
-    }
-    return candidate;
-}}
- //Boyee Moree Voting Algorithim
-/*
-int candidate = nums[0];
-   int count = 1;
-   for(int i = 1;i<nums.length;i++){
-      if(count ==0){
-        candidate = nums[i];
-        count =1;
-      }
-      else if(candidate == nums[i]){
-        count++;
-      }else{
-        count--;
-      }
-   }
-    return candidate;    
-       
-    }
-
-*/    
-
-
-
-
- /* 
- Using Hasmap we solved it 
- On and space oN 
- 
- Map<Integer,Integer> myMap = new HashMap<>();
-        
-        int maxCount = 0;
-        int maxElement = 0;
-        for(int i =0;i<nums.length;i++){
-            myMap.put(nums[i],myMap.getOrDefault(nums[i],0)+1);
-        }
-
-
-        for(Map.Entry<Integer,Integer> entry : myMap.entrySet()){
-            if(entry.getValue()>maxCount){
-                maxCount = entry.getValue();
-                maxElement = entry.getKey();
+        for(int i =1;i<nums.length;i++){
+            if(count ==0){
+                candidate = nums[i];
+            }
+            if(candidate == nums[i]){
+                count++;
+            }else {
+                count--;
             }
         }
-        return maxElement;
-        */
+        return candidate;
+
+    }
+}
