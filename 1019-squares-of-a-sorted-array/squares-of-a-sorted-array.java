173cc032821d2a1723ec8a25311598811d593a05
Nishank Bansal <80453652+nishank23@users.noreply.github.com>
2025-12-18 07:33:54 +0200

    Time: 1 ms (100.00%) | Memory: 47.5 MB (38.91%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,55 +1,24 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-
-        int[] ans = new int [nums.length];
-        int n = nums.length;
-        int r = n-1;
         int i =0;
-        int j = n-1;
+        int j =nums.length-1;
+        int r = j;
+        int ans[] = new int[r+1];
         while(r>=0){
-        int sq1 = nums[i]*nums[i];
-        int sq2 = nums[j]*nums[j];
-        if(sq1>sq2){
-            ans[r] = sq1;
-            i++;
-        }else{
-            ans[r] = sq2;
-            j--;
+            int s1= nums[i] * nums[i];
+            int s2= nums[j] * nums[j];
+
+            if(s2>s1){
+                ans[r] = s2;
+                j--;
+            }else{
+                ans[r] = s1;
+                i++;
+            }
+            r--;
+
+
         }
-        r--;    
-        }
-
-
-
         return ans;
     }
 }
-
-
-/*
- int i =0;
-       int n = nums.length;
-       int j =n-1;
-       int[] ans = new int[n];
-       int r = n-1;
-       while(r>=0){
-           int sq1 = nums[i]*nums[i];
-           int sq2 = nums[j]*nums[j];
-
-            if(sq1>sq2){  
-                ans[r] = sq1;
-                i++;
-            }else{
-                ans[r] = sq2;
-                j--;
-            }
-            r--;
-            // if(i==j){
-            //     ans[r]=(int) Math.pow(nums[i],2);
-            // }
-
-       }
-       return ans;
-
-
-*/
