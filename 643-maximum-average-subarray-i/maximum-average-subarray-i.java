858eaf01a77a8fe6efd2bf78304d7f322ef7019a
Nishank Bansal <80453652+nishank23@users.noreply.github.com>
2025-12-18 07:20:57 +0200

    Time: 2 ms (99.68%) | Memory: 69.5 MB (39.30%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,54 +1,17 @@
 class Solution {
-    public double findMaxAverage(int[] nums, int k) {
-           int bestSum = 0;
-           int currentSum =0;
-           for(int i =0;i<k;i++){
-            currentSum = currentSum + nums[i];
-           }
-           bestSum = currentSum;
-           int n =nums.length; 
-            for(int i = k;i<n;i++){   
-
-                currentSum = currentSum + nums[i]- nums[i-k];
-                if(currentSum>bestSum){
-                    bestSum = currentSum;
-                }
-
+     public double findMaxAverage(int[] nums, int k) {
+        // [1,12,-5,-6,50,3], k = 4
+        int currSum = 0;
+        for (int i = 0; i < k; i++) {
+            currSum += nums[i];
+        }
+        int bestSum = currSum;
+        for(int i = k ;i<nums.length;i++){
+            currSum = currSum - nums[i-k] + nums[i];
+            if(currSum>bestSum){
+                bestSum= currSum;
             }
-
-        
+        }
         return (double) bestSum/k;
-
     }
 }
-
-
-
-
-
-/*
- int bestSum = 0;
-        int currentSum = 0;
-        int n =nums.length;
-        if(n==1){
-            return nums[0];
-        }
-        for(int i =0;i<k;i++){
-            currentSum = currentSum+nums[i];
-        }
-        // for the edge cases if our subarray has n and k is also n then we need to put the value 
-        // what if that very first window has largest sum of all 
-        bestSum = currentSum;
-        for(int i = k ;i<n;i++){
-            currentSum = currentSum - nums[i-k] + nums[i];
-            if(currentSum>bestSum){
-                bestSum = currentSum;
-            }
-        }
-
-        
-
-        return (double) bestSum/k; 
-
-
- */       
