549a6091aa40fefd65af97155f497e66a4bcd731
Nishank Bansal <80453652+nishank23@users.noreply.github.com>
2025-12-21 19:31:46 +0200

    Time: 3 ms (0.31%) | Memory: 43 MB (19.88%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -10,18 +10,19 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        int size  = 0;
+       int size  = 0;
         ListNode curr = head;
         while(curr!=null){
          curr =curr.next;
          size++;
         }
         size = (size/2);
+        System.out.println("Size of the current list "+size);
         while(size!=0) {
             head = head.next;
             size--;
         }
 
-        return head;
+        return head; 
     }
 }
