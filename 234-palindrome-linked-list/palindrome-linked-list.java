ba886447187c5b522c49ef950333d3204a77fee7
Nishank Bansal <80453652+nishank23@users.noreply.github.com>
2025-12-24 12:00:58 +0200

    Time: 4 ms (81.71%) | Memory: 94.5 MB (42.35%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,43 +10,44 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-         if(head==null || head.next ==null){
+          if(head == null || head.next == null){
             return true;
         }
-       
-        ListNode fast = head;
-        ListNode slow = head;
-
-        int size=0;
-        while(head!=null){
-            head = head.next;
+        
+        ListNode lengthNode = head;
+        int size = 0;
+        while(lengthNode!=null){
+            lengthNode=lengthNode.next;
             size++;
         }
         size = size/2;
+        ListNode firstHalfPointer = head;
         while(size!=0){
-            fast = fast.next;
+            firstHalfPointer = firstHalfPointer.next;
             size--;
         }
-
-        // 1 -> 2 -> 3 -> null
-
-        // null -> 3 -> 2 -> 1
-        ListNode prev2= null;
-        while(fast!=null){
-            ListNode temp = fast.next;
-            fast.next = prev2;
-            prev2 = fast;
-            fast = temp;
+    
+        ListNode reverseFirstHalf = null;
+        
+        
+        while(firstHalfPointer!=null){
+            ListNode temp = firstHalfPointer.next;
+            firstHalfPointer.next = reverseFirstHalf;
+            reverseFirstHalf = firstHalfPointer;
+            firstHalfPointer = temp;
         }
-        while(prev2!=null){
-            if(prev2.val!=slow.val){
+        
+        ListNode checkPointer = head;
+        
+        
+        while(reverseFirstHalf!=null){
+            if(reverseFirstHalf.val != checkPointer.val){
                 return false;
             }
-            slow = slow.next;
-            prev2 = prev2.next;
+            checkPointer = checkPointer.next;
+            reverseFirstHalf = reverseFirstHalf.next;
         }
-
         return true;
-
+        
     }
 }
