bde5cb808b47759846369a3d278c7895fd1f460b
Nishank Bansal <80453652+nishank23@users.noreply.github.com>
2025-12-26 06:10:31 +0200

    Time: 22 ms (74.15%) | Memory: 49.1 MB (85.06%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -10,27 +10,54 @@
  */
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
+        // ArrayList<Integer> myArrayList = new ArrayList<>();
+        // while(head!=null){
+        //     if(head.next!=null){
+        //         ListNode greater = head.next;
+        //         while(greater!=null && greater.val<=head.val){
+        //             greater = greater.next;
+        //         }
+        //         if(greater!=null){
+        //             myArrayList.add(greater.val);
+        //         }else{
+        //             myArrayList.add(0);
+        //         }
+        //     }else{
+        //         myArrayList.add(0);
+        //     }
+        //     head=head.next;
+        // } 
+        // int[] ans = new int[myArrayList.size()];
+        // for(int i =0;i<myArrayList.size();i++){
+        //     ans[i] = myArrayList.get(i);
+        // }
+        // return ans;
+
+
+
         ArrayList<Integer> myArrayList = new ArrayList<>();
         while(head!=null){
-            if(head.next!=null){
-                ListNode greater = head.next;
-                while(greater!=null && greater.val<=head.val){
-                    greater = greater.next;
-                }
-                if(greater!=null){
-                    myArrayList.add(greater.val);
-                }else{
-                    myArrayList.add(0);
-                }
-            }else{
-                myArrayList.add(0);
-            }
-            head=head.next;
-        } 
-        int[] ans = new int[myArrayList.size()];
-        for(int i =0;i<myArrayList.size();i++){
-            ans[i] = myArrayList.get(i);
+            myArrayList.add(head.val);
+            head = head.next;
         }
-        return ans;
+        int[] result = new int[myArrayList.size()];
+        Stack<Integer> myStack = new Stack<>();
+
+
+        for(int i =0;i<myArrayList.size();i++){
+
+            while(!myStack.isEmpty() && myArrayList.get(i) > myArrayList.get(myStack.peek())){
+                int index = myStack.pop();
+                result[index] = myArrayList.get(i);
+            }
+            myStack.push(i);
+        }
+
+
+        return result;
+
+
+
+
     }
 }
